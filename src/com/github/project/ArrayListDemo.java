2867546a7ac8aa0b3767820cfaf859ea1b03d736
samikshanagone <samikshanagone33@gmail.com>
2022-12-24 19:04:15 +0530

    first commit : added arraylist demo code

---
+++ src/com/github/project/ArrayListDemo.java
@@ -0,0 +1,14 @@
+package com.github.project;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+public static void main(String[] args) {
+	ArrayList<String> list=new ArrayList<String>();
+	list.add("samiksha");
+	list.add("mayuri");
+	list.add("sita");
+	list.add("gita");
+	System.out.println(list);
+}
+}
